e557c0eae9f67977d66b88c5930b3b675d0b3c9c
Micaheal Johnston <johnston.michaeld@yahoo.com>
2022-08-31 11:17:54 -0600

    added Button componant, adjusted Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
-    <header>
-        <h1>{title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add"
+      onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker 9000',
-}
+  title: "Task Tracker 9000",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
