6ecde4b08b6b93970fb59d524c878f66ad237ceb
Micaheal Johnston <johnston.michaeld@yahoo.com>
2022-08-31 18:43:19 -0600

    tasks can be added, reminders applied

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor appt.",
+      day: "feb 5 at 3pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Trainer appt.",
+      day: "feb 6 at 7am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Hike",
+      day: "feb 7 at 11am",
+      reminder: true,
+    },
+  ]);
+
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks remain"
+      )}
     </div>
   );
 }
