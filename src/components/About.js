19253c9749a8e1e87317aa6a42731dd85b40e328
Micaheal Johnston <johnston.michaeld@yahoo.com>
2022-09-07 16:10:34 -0600

    added about sentance

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div>
     <h4>Version 1.0.0</h4>
-    <Link to="/">Go Back</Link>
+    <br />
+    <p>A small webapp to learn and practice HTML/CSS/ReactJS.</p>
+
+    <Link to="/">Close</Link>
     </div>
   )
 }
